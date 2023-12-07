76dcfb9b2e3115329b72c4a99d01455452343699
Quentin Santos <qsantos@qsantos.fr>
2023-12-07 08:46:50 +0100

    Add pythonrc.py

---
+++ pythonrc.py
@@ -0,0 +1,16 @@
+from math import *
+from builtins import pow
+from random import *
+from string import *
+from datetime import *
+from itertools import *
+from collections import *
+
+import re
+import sys
+import math
+import json
+import random
+import string
+import itertools
+import collections
