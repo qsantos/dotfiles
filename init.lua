236bbe0a57507816dc8427f6a8abcaae4b2450aa
Quentin Santos <qsantos@qsantos.fr>
2025-12-16 18:40:22 +0100

    init.lua: restore cursor position

--- init.lua
+++ init.lua
@@ -260,6 +260,28 @@ else
   vim.notify("Persistent undo disabled: no writable undodir", vim.log.levels.WARN)
 end
 
+local autocmd = vim.api.nvim_create_autocmd
+
+-- @returns a "clear = true" augroup
+local function augroup(name) return vim.api.nvim_create_augroup('sergio-lazyvim_' .. name, { clear = true }) end
+
+autocmd('BufReadPost', {
+  group = augroup('restore_position'),
+  callback = function()
+    local exclude = { 'gitcommit' }
+    local buf = vim.api.nvim_get_current_buf()
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then return end
+
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local line_count = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= line_count then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      vim.api.nvim_feedkeys('zvzz', 'n', true)
+    end
+  end,
+  desc = 'Restore cursor position after reopening file',
+})
+
 -- nvim-cmp
 local cmp = require("cmp")
 local luasnip = require("luasnip")
